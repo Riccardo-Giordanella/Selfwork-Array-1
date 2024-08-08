669a37bda0060c032c8a965e3bfedf8736c77a27
Riccardo Giordanella <it.gg@hotmail.it>
2024-08-09 01:23:41 +0200

    Fine esercizio

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
-let array1 = [3, 7, -2, 5, 8, 1, 2, 5, 6, -4, 12, 25];
-let array2 = [3, 7, -2, 5, 8, 1, 2, 5, 6, -4, 12, 25];
-
+let numbers = [3, 7, -2, 5, 8, 1, 2, 5, 6, -4, 12, 25];
 // Ordinamento decrescente
-const decreasing = array1.sort((a, b)=> b - a);
+let decreasing = numbers.sort((a, b)=> b - a);
 
 console.log(decreasing);
 
 // Ordinamento crescente
-const increasing = array2.sort((a, b)=> a - b);
+let increasing = numbers.sort((a, b)=> a - b);
 
 console.log(increasing);
 
